c06378658f18f740942f71d4456ed97fb8f9a160
Mauro Lepore <maurolepore@gmail.com>
2021-02-16 14:40:10 +0000

    Prune _targets.R

--- _targets.R
+++ _targets.R
@@ -2,26 +2,10 @@ library(targets)
 
 source(file.path("R", "functions.R"))
 
-# Not clean to me
-# * class
-# * iteration
-# * file
-
-tar_option_set(
-  packages = "tidyverse",
-
-  # Declare the rules that mark a target as outdated.
-  cue = tar_cue(
-    # mode = "thorough",
-    # command = TRUE,
-    # depend = TRUE,
-    # format = TRUE
-  ),
-  format = "rds"
-)
+tar_option_set(packages = "tidyverse")
 
 list(
   tar_target(path, "data/raw_data.csv", format = "file"),
-  tar_target(raw, read(path)),
-  tar_target(data, clean(raw), cue = tar_cue(depend = TRUE))
+  tar_target(raw, command = read(path)),
+  tar_target(data, clean(raw))
 )
