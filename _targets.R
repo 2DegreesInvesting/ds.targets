668a290aa4ec95ca1ba33e81593bd322c37b315a
Mauro Lepore <maurolepore@gmail.com>
2021-02-23 18:54:18 +0000

    Fix use of

--- _targets.R
+++ _targets.R
@@ -1,13 +1,8 @@
 library(targets)
 
-# source(file.path("R", "read.R"))
-# source(file.path("R", "clean.R"))
-# ...
-
-# tar_option_set(packages = "tidyverse")
-tar_option_set(envir = getNamespace("ds.targets"))
-# tar_option_set(envir = getNamespace("ds.targets"), imports = c("pkg1", "pkg2"))
-
+tar_option_set(imports = "ds.targets", packages = "ds.targets")
+# Or
+# tar_option_set(envir = getNamespace("ds.targets"))
 
 list(
   tar_target(path, raw_path(), format = "file"),
