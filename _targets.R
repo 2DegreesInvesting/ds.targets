5ab872bad4c259e3f83fedd033844b3a0a1df4e4
Mauro Lepore <maurolepore@gmail.com>
2021-03-01 23:40:50 +0000

    Prepare for meetup

--- _targets.R
+++ _targets.R
@@ -2,16 +2,18 @@ library(targets)
 library(tarchetypes)
 
 options(TZ="Germany/Berlin")
-tar_option_set(imports = "ds.targets", packages = c("ds.targets", "tidyverse"))
+tar_option_set(imports = "ds.targets", packages = c("ds.targets"))
 
 list(
   # External
   tar_target(path, raw_path(), format = "file"),
+
   # Internal: _targets/objects/
   tar_target(raw, command = read(path)),
   tar_target(data, clean(raw)),
   tar_target(model, linear_model(data)),
+
   # External
-  tar_render(lm, "lm.Rmd", output_format = "md_document"),
-  tar_render(plot, "plot.Rmd", output_format = "md_document")
+  tarchetypes::tar_render(lm, "lm.Rmd", output_format = "md_document"),
+  tarchetypes::tar_render(plot, "plot.Rmd", output_format = "md_document")
 )
